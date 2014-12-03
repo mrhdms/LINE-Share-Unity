bcd9413b54f1fb4de3cd2d60cda462f0e73315af
mrhdms <mrhdms@gmail.com>
2014-12-03 23:03:46 +0900

    プラグイン連携部分のファイルを分けた

--- Assets/LINE/LINE.cs
+++ Assets/LINE/LINE.cs
@@ -1,51 +1,43 @@
 ﻿using UnityEngine;
 using System.Runtime.InteropServices;
 
-public class LINE
-{
-		private const string URI = "line://msg/{0}/{1}";
+public class LINE {
+	private const string URI = "line://msg/{0}/{1}";
 
-		/// <summary>
-		/// LINEでテキストをシェアします
-		/// </summary>
-		/// <param name="message">シェアするメッセージ</param>
-		public static void ShareText (string message)
-		{
-				Application.OpenURL (string.Format (URI, "text", System.Uri.EscapeDataString (message)));
-		}
+	/// <summary>
+	/// LINEでテキストをシェアします
+	/// </summary>
+	/// <param name="message">シェアするメッセージ</param>
+	public static void ShareText(string message) {
+		Application.OpenURL(string.Format(URI, "text", System.Uri.EscapeDataString(message)));
+	}
 
-		/// <summary>
-		/// LINEで画像をシェアします
-		/// </summary>
-		/// <param name="textureURL">シェアしたい画像のローカルパス</param>
-		public static void ShareImage (string textureURL)
-		{
-				ShareImageSub (textureURL);
-		}
-		#if UNITY_IPHONE && !UNITY_EDITOR
-	[DllImport ("__Internal")]
-	static extern string _SetImage(string textureURL) ;
-
+	/// <summary>
+	/// LINEで画像をシェアします
+	/// </summary>
+	/// <param name="textureURL">シェアしたい画像のローカルパス</param>
+	public static void ShareImage(string textureURL) {
+		ShareImageSub(textureURL);
+	}
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	
 	private static void ShareImageSub(string textureURL) {
-		string shareImageString = _SetImage(textureURL);
+		string shareImageString = CopyClipBoard.Image(textureURL);
 		Application.OpenURL(string.Format(URI, "image", WWW.EscapeURL(shareImageString)));
 	}
 
 	
-
 #elif UNITY_ANDROID && !UNITY_EDITOR
-		
+	
 	private static void ShareImageSub(string textureURL) {
 		string shareImageString = textureURL;
 		Application.OpenURL(string.Format(URI, "image", shareImageString));//androidはエスケープしなくて良い
 	}
 
 	
-
 #else
-		private static void ShareImageSub (string textureURL)
-		{
-				Debug.Log ("PCでは動作しません。" + textureURL + "を指定しました。");
-		}
-		#endif
+	private static void ShareImageSub(string textureURL) {
+		Debug.Log("PCでは動作しません。" + textureURL + "を指定しました。");
+	}
+	#endif
 }
---
+++ Assets/Plugins/CopyClipBoard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class CopyClipBoard {
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	static extern string _SetImage(string textureURL) ;
+	[DllImport ("__Internal")]
+	static extern string _SetText(string text) ;
+	#endif
+
+	public static void Text(string text) {
+		#if UNITY_IPHONE && !UNITY_EDITOR
+		string shareImageString = _SetText(text);
+		#elif UNITY_ANDROID && !UNITY_EDITOR
+	AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	AndroidJavaObject activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+	activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+	AndroidJavaObject clipboardManager = activity.Call<AndroidJavaObject>("getSystemService", "clipboard");
+	//clipboardManager.Call("setText", exportData);
+	AndroidJavaClass clipDataClass = new AndroidJavaClass("android.content.ClipData");
+	AndroidJavaObject clipData = clipDataClass.CallStatic<AndroidJavaObject>("newPlainText", "simple text", text);
+	clipboardManager.Call("setPrimaryClip", clipData);
+	}));
+		#else
+		Debug.Log("PCではコピーできませんが、" + text + " がクリップボードにコピーされます。");
+		#endif
+	}
+
+	public static string Image(string textureURL) {
+		#if UNITY_IPHONE && !UNITY_EDITOR
+		return _SetImage(textureURL);
+		#elif UNITY_ANDROID
+		Debug.LogError("未実装です");
+		return "";
+		#else
+		Debug.Log("PCではコピーできませんが、" + textureURL + " がクリップボードにコピーされます。");
+		return "";
+		#endif
+	}
+}
